91d37f404c790742e45bbf166f2a7c4704d2ff56
SoumyaPanda <soumyaranjanpanda988@gmail.com>
2024-06-03 13:57:50 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,17 @@ import { useEffect, useState } from "react";
 import { CreateTodo } from "./components/CreateTodo";
 import axios from "axios";
 import { Todo } from "./components/Todo";
+//https://sum-server.100xdevs.com/todos
+//"http://localhost:3000/api/v1/todos"
 function App() {
   const [todos, setTodos] = useState([]);
+  // const fetchFnc = async () => {
+  //   const response = axios.get("https://sum-server.100xdevs.com/todos")
+  //   .then((data)=>{
+  //     console.log(data.data.response);
+  //     setTodos(data.data.response)
+  //   }) 
+  // }
   const fetchFnc = () => {
     fetch("http://localhost:3000/api/v1/todos").then(async (res) => {
       const json = await res.json();
